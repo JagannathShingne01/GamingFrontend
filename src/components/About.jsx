b735f7d74b44693635cdabefe980a4476922951f
JagannathShingne01 <jagannathshingne01@gmail.com>
2024-12-03 12:34:50 +0530

    About section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,37 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import React from 'react'
+import { ScrollTrigger } from 'gsap/all';
+import AnimatedTitle from './AnimatedTitle';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+          scrollTrigger: {
+            trigger: "#clip",
+            start: "center center",
+            end: "+=800 center",
+            scrub: 0.5,
+            pin: true,
+            pinSpacing: true,
+          },
+        });
+    
+        clipAnimation.to(".mask-clip-path", {
+          width: "100vw",
+          height: "100vh",
+          borderRadius: 0,
+        });
+      });
   return (
     <div id='about' className='min-h-screen w-screen'>
         <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
             <h2 className='font-general text-sm uppercase md:text-[10px]'>
         welcome to Zentry
             </h2>
-            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-        Disc<b>o</b>ver the world&apos;s <br/> l<b>a</b>rgest shared adventure
-            </div>
+            <AnimatedTitle title="Disc<b>o</b>ver the worlds <br/> l<b>a</b>rgest shared adventure" containerClass="mt-5 !text-black text-center"/>
             <div className='about-subtext'>
                 <p>The Game of Game begins-your life, now an epic MMORPG</p>
                 <p>Zentry unites every player from countless games and platforms</p>
